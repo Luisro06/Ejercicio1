38b354dccfeeff6b3444d75ce45578d49d7fe603
Melisa <melisadmendizabal@gmail.com>
2023-08-04 09:03:04 -0600

    Declarar variables

--- IU.java
+++ IU.java
@@ -1,3 +1,16 @@
 public class IU {
+    String word;
+    Boolean palindromo;
+
+    public static void mostrarResultado() {
+        
+    }
     
+    public static void cerrar() {
+
+    }
+
+    private boolean recibirCompPal(Boolean palindromo) {
+        return 
+    }
 }
